15c37c6b3d5c32d83870c4479e617eb33e641993
Spyros Pikoulas <s230284@dtu.dk>
2024-01-06 18:50:53 +0100

    training

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -18,58 +18,42 @@ logging.basicConfig(level=logging.INFO,
 
 logger=logging.getLogger(__name__)
 
-def processing(df):
-    df = df.fillna('No info', axis=1)
-    df['input'] = 'KEYWORD: ' + df.keyword + '; LOCATION: ' + df.location + '; TEXT: ' + df.text
-    df['input'] = df['input'].str.lower()
-    df = df.drop(columns=['keyword','location','text'], axis=1)
-    df.fillna("no info", inplace=True)
+
+def find_project_dir() -> str:
+    return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+def processing(df: pd.DataFrame) -> pd.DataFrame:
+    # Drop stuff
+    df = df.dropna()
+    df = df.drop(columns=['keyword','location','id'], axis=1)
     df.drop_duplicates(inplace=True)
+
+    # Process
+    df['text'] = df['text'].str.lower()
+    df = df.rename(columns={"target": "labels"})
+    df = df.reset_index(drop=True)
+    print(df.head(5))
+
     return df
 
-class DisasterTweets():
-    def __init__(self):
-        project_directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        self.data_path  = f"{project_directory}/data/raw/"
-        self.load_raw()
-        self.process_datasets()
+def preprocess_data(file, store=False) -> pd.DataFrame:
+    # Get Data
+    project_directory = find_project_dir()
+    data_path  = f"{project_directory}/data/raw/"
+    train_df = pd.read_csv(f'{data_path}{file}')
 
-    def load_raw(self):
-        self.train = pd.read_csv(f'{self.data_path}train.csv')       
-        logger.info(f'train columns: {self.train.columns.tolist()}')
-        logger.info(f'train dataset length: {len(self.train)}')
-        
-    def process_datasets(self):
-        self.train = processing(self.train)
-        self.train['target'] = np.float64(self.train['target'])
+    # Process it
+    logger.info(f'Preprocessing dataframe of shape: {train_df.shape}')
+    train_df = processing(train_df)
 
-class LitDM(lit.LightningDataModule):
-    def __init__(self, data_path: str='data/processed', batch_size: int = 32):
-        super().__init__()
-       
-        self.data_path = data_path
-        self.batch_size = batch_size
-        self.cpu_cnt = os.cpu_count() or 2
+    # Store it
+    if store:
+        train_df.to_pickle(f"{project_directory}/data/processed/{file}.pkl")
 
-    def setup(self, stage: Optional[str] = None) -> None:
-        train_inputs = torch.load('data/processed/train_inputs.pt')
-        validation_inputs= torch.load('data/processed/validation_inputs.pt')
+    return train_df
 
-        train_labels = torch.load('data/processed/train_labels.pt')
-        validation_labels = torch.load('data/processed/validation_labels.pt')
+def load_train_df(file: str) -> pd.DataFrame:
+    project_directory = find_project_dir()
+    train_df = pd.read_pickle(f"{project_directory}/data/processed/{file}")
 
-        train_masks =torch.load('data/processed/train_masks.pt')
-        validation_masks = torch.load('data/processed/validation_masks.pt')
-     
-        self.train_data = TensorDataset(train_inputs, train_masks, train_labels)
-        self.validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
-        self.validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
-
-    def train_dataloader(self) -> DataLoader:
-        return DataLoader(self.train_data, batch_size=self.batch_size, num_workers=self.cpu_cnt)
-        
-    def test_dataloader(self) -> DataLoader:
-        pass
-
-    def val_dataloader(self) -> DataLoader:
-        return DataLoader(self.validation_data, batch_size=self.batch_size, num_workers=self.cpu_cnt)
+    return train_df
--- src/train_model.py
+++ src/train_model.py
@@ -1,18 +1,79 @@
+from data.dataset import preprocess_data
+from sklearn.model_selection import train_test_split
+from datasets import Dataset, DatasetDict
+from transformers import DataCollatorWithPadding, AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+from sklearn.metrics import accuracy_score
 import numpy as np
-import torch
-from data.dataset import LitDM
-from transformers import BertForSequenceClassification, AdamW, BertConfig, get_linear_schedule_with_warmup
-import time
-import datetime
-import random
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return {"accuracy": accuracy_score(labels, predictions)}
 
 if __name__ == '__main__':
-    dm = LitDM()
-    dm.setup()
+    # Preprocess(or load) and get data
+    train_df = preprocess_data("train.csv", store=True)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Split training data
+    train_df, test_df = train_test_split(train_df, test_size=0.2)
 
-    train_dataloader = dm.train_dataloader()
-    validation_dataloader = dm.val_dataloader()
+    # Build Datasets
+    train_dataset = Dataset.from_pandas(train_df[:100])
+    test_dataset = Dataset.from_pandas(test_df[:100])
+    dataset_dict = DatasetDict({
+        'train': train_dataset,
+        'test': test_dataset
+    })
 
-    print(type(train_dataloader))
+    # Tokenize
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+    def preprocess_function(examples):
+        return tokenizer(examples["text"], truncation=True)
+    tokenized_data = dataset_dict.map(preprocess_function)
+    # Padding by batch
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    # Model
+    id2label = {0: "NO DISASTER", 1: "DISASTER"}
+    label2id = {"NO DISASTER": 0, "DISASTER": 1}
+    model = AutoModelForSequenceClassification.from_pretrained(
+        "bert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+    )
+
+    # Training arguments
+    training_args = TrainingArguments(
+    output_dir="../models/",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    logging_strategy="steps",  # Log every X steps
+    logging_steps=1,
+    load_best_model_at_end=True,
+    push_to_hub=False,
+    )
+
+    # Trainer
+    trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_data["train"],
+    eval_dataset=tokenized_data["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
+
+
+
+
+
+
+
+
+
