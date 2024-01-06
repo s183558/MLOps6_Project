b559c638d2df751e427f0b7fffa84b2af68c53b6
Andreas Skov Millarch <andreasmillarch@gmail.com>
2024-01-06 11:08:21 +0100

    added raw data, updated make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,3 +1,23 @@
-if __name__ == '__main__':
-    # Get the data and process it
-    pass
+import logging
+import pandas as pd
+
+class DisasterTweets():
+    def __init__(self):
+        self.data_path  = "data/raw/"
+        
+    def load_raw(self):
+        self.train = pd.read_csv(f'{self.data_path}train.csv')
+        self.test = pd.read_csv(f'{self.data_path}test.csv')
+        
+        logger.info(f'columns: {self.train.columns.tolist()}')
+        logger.info(f'train dataset length: {len(self.train)}, test dataset length: {len(self.test)}')
+
+if __name__=="__main__":  
+    
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                        datefmt='%m-%d %H:%M')
+  
+    logger=logging.getLogger(__name__)
+    ds = DisasterTweets()
+    ds.load_raw()
