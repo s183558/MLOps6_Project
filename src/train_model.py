083903c269b3736deeb5da2e4306060e2b34b3ad
Spyros Pikoulas <s230284@dtu.dk>
2024-01-10 17:34:37 +0100

    Trainer params

--- src/train_model.py
+++ src/train_model.py
@@ -31,8 +31,17 @@ if __name__ == '__main__':
 
     # Training
     epochs = cfg.model["epochs"]
-    trainer = Trainer(max_epochs=cfg.model["epochs"],
-                      callbacks=[EarlyStopping(monitor="val_loss", mode="min")])
+    trainer = Trainer(
+        max_epochs=cfg.model["epochs"],
+        callbacks=[EarlyStopping(monitor="val_loss", mode="min"),
+        check_val_every_n_epoch=1, # Evaluate after every epoch
+        enable_checkpointing = False, # Model checkpoints
+        limit_train_batches=0.2, # Train at only 20% of the data
+        #limit_val_batches=0.2,
+        num_sanity_val_steps=0, # Do not perform sanity check
+        #profiler="simple",
+        precision="16-true", # Drop from float 32 to float 16 precision for memory efficiency
+    ])
     
     trainer.fit(model, dm)
 
