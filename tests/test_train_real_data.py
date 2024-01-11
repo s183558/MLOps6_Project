953d241d903a1969305dbabb4228ee6737fb41ab
s183558 <s183558@student.dtu.dk>
2024-01-11 13:04:39 +0100

    Update test_train_real_data.py with new model configurations

--- tests/test_train_real_data.py
+++ tests/test_train_real_data.py
@@ -3,27 +3,39 @@ from src.train_model import train_main
 from transformers import AutoTokenizer
 from pytorch_lightning import Trainer
 from omegaconf import DictConfig
-from tests import _PATH_DATA
+import os.path
 import pytest
-import os
+from tests import _PATH_DATA
 
 @pytest.mark.skipif(not os.path.exists(_PATH_DATA), reason="Data files not found")
-@pytest.mark.parametrize("precision", ["32-true", "16-true"])
+#@pytest.mark.parametrize("precision", ["32-true", "16-true"])
 def test_train_main():
     # The configurations for the tests
     cfg = DictConfig({
+        'data': {
+            'test_size': 0.1,
+            'valid_size': 0.2,
+            'seed': 42,
+            'token_max_length': 128,
+            'batch_size': 32,
+        },
         'model': {
-            'lr': 0.001,
+            'output_dir': 'models/',
             'optimizer': 'Adam',
+            'lr': 2e-5,
             'epochs': 2,
-            'limit_train_batches': 0.2,
-            'limit_val_batches': 0.2,
-            'mixed_precision': 32
-        }
-    })
+
+            'per_device_train_batch_size': 16,
+            'per_device_eval_batch_size': 16,
+            'weight_decay': 0.01,
+
+            'limit_train_batches': 0.05,
+            'limit_val_batches': 0.05,
+
+            'mixed_precision': "32-true",
+        }})
 
     # Call the function
     train_main(cfg)
 
     #
-    
