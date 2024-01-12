b39e1ee65775e3fe02207a9939dc3e7b113f2c27
s183558 <s183558@student.dtu.dk>
2024-01-12 10:09:03 +0100

    Refactor unit tests to use Hydra for configuration

--- tests/test_train_mock.py
+++ tests/test_train_mock.py
@@ -3,32 +3,16 @@ from unittest.mock import patch, MagicMock
 from src.train_model import train_main
 from transformers import AutoTokenizer
 from pytorch_lightning import Trainer
-from omegaconf import DictConfig
+from hydra import initialize, compose
 
 def test_train_main():
-    # Mock the configuration
-    cfg = DictConfig({
-        'model': {
-            'lr': 0.001,
-            'optimizer': 'Adam',
-            'epochs': 100,
-            'limit_train_batches': 1,
-            'limit_val_batches': 1,
-            'limit_test_batches': 1,
-            'mixed_precision': 32,
-            'wandb_logging': 'disabled',
-            'outputdir': 'models/'
-        },
-        
-    })
-
     # Mock the tokenizer, data module, model, and trainer
     with patch.object(AutoTokenizer, 'from_pretrained') as mock_tokenizer, \
-         patch('src.train_model.LitDM') as mock_dm, \
-         patch('src.train_model.AlbertClassifier') as mock_model, \
-         patch.object(Trainer, '__init__') as mock_trainer_init, \
-         patch.object(Trainer, 'fit') as mock_trainer_fit, \
-         patch.object(Trainer, 'test') as mock_trainer_test:
+        patch('src.train_model.LitDM') as mock_dm, \
+        patch('src.train_model.AlbertClassifier') as mock_model, \
+        patch.object(Trainer, '__init__') as mock_trainer_init, \
+        patch.object(Trainer, 'fit') as mock_trainer_fit, \
+        patch.object(Trainer, 'test') as mock_trainer_test:
 
         # Set the return values of the mocks
         mock_tokenizer.return_value = MagicMock()
@@ -36,8 +20,10 @@ def test_train_main():
         mock_model.return_value = MagicMock()
         mock_trainer_init.return_value = None
 
-        # Call the function
-        train_main(cfg)
+        # Call the function with test config
+        with initialize(version_base=None, config_path="../conf"):
+            cfg = compose(config_name="config_tests.yaml")
+            train_main(cfg)
 
         # Verify that the mocks were called with the correct arguments
         mock_tokenizer.assert_called_once_with('albert-base-v1')
--- tests/test_train_real_data.py
+++ tests/test_train_real_data.py
@@ -1,34 +1,10 @@
 from src.train_model import train_main
-from omegaconf import DictConfig
+from hydra import initialize, compose
 
 #@pytest.mark.parametrize("precision", ["32-true", "16-true"])
 def test_train_main():
-    # The configurations for the tests
-    cfg = DictConfig({
-        'data': {
-            'test_size': 0.1,
-            'valid_size': 0.2,
-            'seed': 42,
-            'token_max_length': 128,
-            'batch_size': 2,
-        },
-        'model': {
-            'output_dir': 'models/',
-            'optimizer': 'Adam',
-            'lr': 2e-5,
-            'epochs': 1,
-            'max_steps': 1,
-
-            'limit_train_batches': 0.0006,
-            'limit_val_batches': 0.003,
-            'limit_test_batches': 0.004,
-
-            'mixed_precision': "32-true",
-            'wandb_logging': "disabled",
-        },
-        
-        })
-
-    # Call the function
-    train_main(cfg)
+    # Call the function with test config
+        with initialize(version_base=None, config_path="../conf"):
+            cfg = compose(config_name="config_tests.yaml")
+            train_main(cfg)
 
