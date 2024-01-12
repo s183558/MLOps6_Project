9e39ee58b2091032a80f25a4911cc8d7309d8ce7
s183558 <s183558@student.dtu.dk>
2024-01-12 17:29:05 +0100

    Refactor predict function

--- src/predict_model.py
+++ src/predict_model.py
@@ -3,14 +3,13 @@ from src.models.model import AlbertClassifier
 from src.data.dataset import LitDM
 import glob
 import os
-import hydra
+from hydra import initialize, compose
 from omegaconf import DictConfig
 from transformers import AutoTokenizer
 from pytorch_lightning import Trainer
 
 
-@hydra.main(version_base=None, config_path="../conf", config_name="config.yaml")
-def predict(cfg:DictConfig):
+def predict(data, config_fname="config.yaml"):
     # Find the latest file in the folder
     model_dir = 'models/lightning_logs/'
     latest_subdir = max(glob.glob(os.path.join(model_dir, '*/')), key=os.path.getmtime)
@@ -19,25 +18,35 @@ def predict(cfg:DictConfig):
     model_names = [f for f in os.listdir(f"{latest_subdir}/checkpoints/") if os.path.isfile(os.path.join(f"{latest_subdir}/checkpoints/", f))][-1]
     model = AlbertClassifier.load_from_checkpoint(f"{latest_subdir}/checkpoints/{model_names}")
 
-    # Create dummy data
-    data = ["movie on my house car","my car is on fire", "elephant was on chicken", "keyboard mouse string", "sandwich are cool lol", "as my own "]
+    
 
     # Specify tokenizer
     tokenizer = AutoTokenizer.from_pretrained('albert-base-v1')
 
     # Get Lit Data Module
+    with initialize(version_base=None, config_path="../conf"):
+        cfg = compose(config_name=config_fname)
     dm = LitDM(cfg, tokenizer = tokenizer)
     dm.prepare_predict(data)
 
     # Predict
-    trainer = Trainer()
+    trainer = Trainer(inference_mode = True)
     output = trainer.predict(model, dm, return_predictions=True)
-    predictions = torch.argmax(output[0]["logits"], dim=1)
+    
+    # Unpack the tensors from their batches, and argmax to get the predicted class
+    prediction_list = []
+    for batch in output:
+        prediction_list.append(torch.argmax(batch["logits"], dim=1))
+    predictions = torch.cat(prediction_list)
+    print(f'predictions: {predictions}')
     print(f'yhat: {["catastrophe" if value == 1 else "not catastrophe" for value in predictions ]}')
 
     return predictions
     
 if __name__ == '__main__':
+    # Create dummy data
+    dummy_data = ["movie on my house car","my car is on fire", "elephant was on chicken",
+            "keyboard mouse string", "sandwich are cool lol", "as my own"]
     print("Started predicting 1 2 3...")
-    model_data = predict()
+    model_data = predict(dummy_data)
     print("Finished predicting.")
