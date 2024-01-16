8fcc19182082ac387c3193b66b8d7f032981ea17
s183558 <s183558@student.dtu.dk>
2024-01-16 15:33:41 +0100

    Update predict function to use a specific model

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -6,7 +6,7 @@ def test_predict():
     dummy_data = ["movie on my house car","my car is on fire", "elephant was on chicken",
             "keyboard mouse string", "sandwich are cool lol", "as my own"]
     print("Started predicting 1 2 3...")
-    model_data = predict(dummy_data, used_for_unit_test=True)
+    model_data = predict(dummy_data, which_model = "random")
 
     assert len(model_data) == len(dummy_data), "Length of input and output should be the same"
     assert type(model_data) == torch.Tensor, "Output should be a tensor"
